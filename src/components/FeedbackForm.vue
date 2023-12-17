f92c7e6fae0ce4594ec6565b03cc9db786086077
MarPls <35261805+MariosPliatsikas@users.noreply.github.com>
2023-12-17 16:45:57 +0100

    Initial commit

--- src/components/FeedbackForm.vue
+++ src/components/FeedbackForm.vue
@@ -1,18 +1,10 @@
-<!-- FeedbackForm.vue -->
 <template>
   <div>
     <h2>Feedback Form</h2>
     <form @submit.prevent="submitFeedback">
-      <label>   
-        Feedback Type:
-        <select v-model="feedbackType">
-          <option value="positive">Positive</option>
-          <option value="negative">Negative</option>
-        </select>
-      </label>
       <label>
-        Comments:
-        <textarea v-model="comments"></textarea>
+        Feedback:
+        <textarea v-model="feedback" required></textarea>
       </label>
       <button type="submit">Submit Feedback</button>
     </form>
@@ -20,30 +12,25 @@
 </template>
 
 <script>
-export default {  
+import axios from 'axios';
+
+export default {
   data() {
     return {
-      feedbackType: 'positive',
-      comments: '',
+      feedback: '',
     };
   },
   methods: {
-    submitFeedback() {
-      // Send feedback data to the backend (to be implemented later)
-      console.log('Feedback submitted:', this.feedbackType, this.comments);
+    async submitFeedback() {
+      try {
+        await axios.post('http://localhost:5000/feedback', { feedback: this.feedback });
+        this.feedback = '';
+        alert('Feedback submitted successfully!');
+      } catch (error) {
+        console.error(error);
+        alert('Failed to submit feedback');
+      }
     },
   },
 };
 </script>
-
-<style scoped>
-/* Your component styles go here */
-
-/* Example styles: Feel free to customize as needed */
-h2 {
-  color: #333;
-  font-size: 24px;
-  margin-bottom: 20px;
-}
-
-</style>
